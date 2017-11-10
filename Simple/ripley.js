b7c289a8f08c7457de5cffc90e0deab174411ea2
Claudio <claudio.sepulveda.geoffroy@gmail.com>
2017-11-10 08:54:25 -0300

    Cambios desde Cyber

--- Simple/ripley.js
+++ Simple/ripley.js
@@ -1,1 +1,6 @@
-// hola Mundo
+// Header New Manifest
+
+// Link servicio a cliente bluemix -> estático
+$(document).ready(() => {
+	$('.footer__links-container a').eq(0).attr('href', 'http://simple.ripley.cl/minisitios/estatico/servicio_cliente/')
+})
